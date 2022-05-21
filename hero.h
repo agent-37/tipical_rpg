a9b1445369ce3b13162d3a9ffc888b94bebeab57
maxim dementyev <maxim.dementev.04@gmail.com>
2022-05-21 10:48:45 +0300

    доделал план на героя

--- hero.h
+++ hero.h
@@ -1,82 +1,93 @@
-#include <iostream>
-#include <algorithm>
-#include <set>
-#include <string>
-#include <iterator>
-#include "person.h"
+#include <iostream> 
+#include <algorithm> 
+#include <set> 
+#include <string> 
+#include <iterator> 
+#include "person.h" 
+
 
 using namespace std;
 
-class hero
+// указатель на функцию  
+//typedef func_ptr(void (Hero::* tmp_ptr)() {}) 
+
+class Inventory
 {
+public:
+
+	void show_weared_artifacts();
+	void show_not_weared_artifacts();
+	void take_off_artifact();
+	void put_on_artifact();
+
+
 private:
-	// координаты поля
-	int x, y;
-	int direction_gaze;
 
 	multiset <string> weapons;
-
 	multiset <string> weared_inventory;
-	/*
-	* 
-	*/
-
 	multiset <string> not_weared_inventory;
-	/*
-	* 	
-	*/
 
+	multiset <string> ::iterator weapons(string thing);
+	multiset <string> ::iterator it_weared(string thing);
+	multiset <string> ::iterator it_not_weared(string thing);
+
+};
+
+class Hero
+{
+private:
+	// координаты поля 
+	int x, y;
+	int direction_gaze;
+	int armor;
+	int artifacts;
 	int count_healing_poison;
-	int num_recovery_units_poison; // позже решим, кол-во хп на сколько регенит зелье
-
-	/*
-	* вопрос: будет ли видно героя во время игры? если нет, то реализация брони будет просто += 1;
-	*/
+	int num_recovery_units_poison; // позже решим, кол-во хп на сколько регенит зелье 
 
 	person user;
 
 public:
-	multiset <string> ::iterator it(string thing);
 
-	// регенерация
+	// регенерация 
 	void healing_poison();
 
 	/*
-	
+
 	допы
 	void use поиск в мультисете
-	void надеть 
+	void надеть
 	void снять
 	class артифакты для инвенторя (классы)
 	void показать, то надето
 	void показать, то снято
-	
+
 	*/
-	// использование оружия
-	void use_weapon();
 
+	// использование оружия 
+	void using_weapon(Weapon& weap);
 	void show_armor();
+	void take_off_armor();
 
-	// шаг вперёд
+	// шаг вперёд 
 	void step_forward() { y += 1; }
 	void (Hero::* step_f)() { &step_forward };
 
-	// шаг вправо
+	// шаг вправо 
 	void step_right() { x += 1; }
 	void (Hero::* step_r)() { &step_right };
 
-	// шаг назад
+	// шаг назад 
 	void step_back() { y -= 1; }
 	void (Hero::* step_b)() { &step_back };
 
-	// шаг влево
+	// шаг влево 
 	void step_left() { x -= 1; }
 	void (Hero::* step_l)() { &step_left };
 
-	// поворот вправо
+	// поворот вправо 
 	void turn_right();
 
-	// поворот влево
+	// поворот влево 
 	void turn_left();
 
 };
@@ -84,93 +95,126 @@ public:
 class Weapon
 {
 public:
-	virtual void use() = 0;
+	virtual void use_weapon() = 0;
 
 };
 
 class Knife :public Weapon
 {
 public:
-	void use() override
+	void use_weapon() override
 	{
-
+		// уточнить реализацию ударов(применения оружия)
 	}
 };
 
 class Gun :public Weapon
 {
 public:
-	void use() override
+	void use_weapon() override
 	{
 
 	}
 };
 
-class Armor
+class Machine_gun :public Weapon
 {
 public:
-	virtual void wear() = 0;
-	virtual void take_off() = 0;
+	void use_weapon() override
+	{
+
+	}
 };
 
-class Helmet :public Armor
+class Club : public Weapon
 {
 public:
-	void wear() override
+	void use_weapon() override
 	{
-		armor += 1; // св дальше реализация будет сложнее
-	}
 
-	void take_off() override
-	{
-		armor -= 1;
 	}
 };
 
-class Bulletproof_vest :public Armor
-{
-public:
-	void wear() override
-	{
-		armor += 1; 
-	}
-
-	void take_off() override
-	{
-		armor -= 1; 
-	}
-};
-
-// добавление элемента и проверка на его наличие
+// добавление элемента и проверка на его наличие 
 multiset <string> ::iterator Hero::it(string thing)
 {
 	multiset <string> ::iterator it_thing;
-	it_thing = inventory.find(thing);
-	inventory.insert(thing);
+	it_thing = weared_inventory.find(thing);
+	weared_inventory.insert(thing);
 	return it_thing;
+}
+
+inline void Hero::using_weapon(Weapon& weap)
+{
+	weap.use_weapon();
+}
+
+inline void Inventory::show_weared_artifacts()
+{
+	for (multiset <string> ::iterator it_weared = weared_inventory.begin(); it_weared != weared_inventory.end(); ++it_weared)
+		cout << *it_weared << endl;
+}
+
+inline void Inventory::show_not_weared_artifacts()
+{
+	for (multiset <string> ::iterator it_not_weared = weared_inventory.begin(); it_not_weared != weared_inventory.end(); ++it_not_weared)
+		cout << *it_not_weared << endl;
+}
+
+inline void Inventory::take_off_artifact()
+{
+	if (weared_inventory.size() > 0)
+	{
+		not_weared_inventory.insert(*(weared_inventory.rbegin()));
+		weared_inventory.erase(--weared_inventory.end());
+	}
+	else
+		cout << "Все артифакты надеты" << endl;
+}
+
+inline void Inventory::put_on_artifact()
+{
+	if (not_weared_inventory.size() > 0)
+	{
+		weared_inventory.insert(*(not_weared_inventory.rbegin()));
+		not_weared_inventory.erase(--not_weared_inventory.end());
+	}
+	else
+		cout << "Все артифакты сняты" << endl;
+}
+
+inline multiset<string>::iterator Inventory::weapons(string thing)
+{
+	return multiset<string>::iterator();
+}
+
+inline multiset<string>::iterator Inventory::it_weared(string thing)
+{
+	return multiset<string>::iterator();
+}
+
+inline multiset<string>::iterator Inventory::it_not_weared(string thing)
+{
+	return multiset<string>::iterator();
 }
 
 void Hero::show_armor()
 {
-	// cout << armor << endl;
+	cout << armor << endl;
 }
 
-void Hero::use_weapon(Weapon &weap)
-{
-	weap->use();
-}
-
-// регенерация
+// регенерация 
 inline void Hero::healing_poison()
 {
 	if (count_healing_poison > 0)
 	{
 		count_healing_poison--;
 	}
-	user.health = max(user.max_health, user.health + num_recovery_units_poison);
+	user.health = max(user.max_health, user.health +
+		num_recovery_units_poison);
 }
 
-// поворот вправо
+// поворот вправо 
 void Hero::turn_right()
 {
 	void (Hero:: * tmp_ptr)() {};
@@ -182,7 +226,7 @@ void Hero::turn_right()
 	step_l = tmp_ptr;
 }
 
-// поворот влево
+// поворот влево 
 void Hero::turn_left()
 {
 	void (Hero:: * tmp_ptr)() {};
@@ -194,8 +238,9 @@ void Hero::turn_left()
 	step_r = tmp_ptr;
 }
 
-//вспомогательная структура для отмотки ходов
-struct hero_and_mark { 
-	hero user;
+//вспомогательная структура для отмотки ходов 
+struct hero_and_mark
+{
+	Hero user;
 	int mark;
 };
