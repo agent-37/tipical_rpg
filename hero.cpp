468ada10ad780ad09771269b7abdfb066bf73a75
email <maxim.dementev.04@gmail.com>
2022-05-24 19:20:15 +0300

    тоже переделал коммментарии

--- hero.cpp
+++ hero.cpp
@@ -11,49 +11,6 @@ using namespace std;
 // указатель на функцию  
 // typedef void (Hero::* func_ptr)();
 
-// герой
-class Hero
-{
-private:
-	int x, y;	// координаты героя на поле
-	int direction_gaze; // направление взгляда
-	int armor; // броня"
-	int count_healing_poison; // количество зелья 
-	int num_recovery_units_poison; // количество восполняемого HP
-	person user;
-
-public:
-
-	// использование зелья 
-	void healing_poison();
-
-	// показ характеристик героя
-	void show_characteristics();
-
-	// шаг вперёд 
-	void step_forward() { y += 1; }
-	void (Hero::* step_f)() { &Hero::step_forward };
-
-	// шаг вправо 
-	void step_right() { x += 1; }
-	void (Hero::* step_r)() { &Hero::step_right };
-
-	// шаг назад 
-	void step_back() { y -= 1; }
-	void (Hero::* step_b)() { &Hero::step_back };
-
-	// шаг влево 
-	void step_left() { x -= 1; }
-	void (Hero::* step_l)() { &Hero::step_left };
-
-	// поворот вправо 
-	void turn_right();
-
-	// поворот влево 
-	void turn_left();
-
-};
-
 // восполнение здоровья
 inline void Hero::healing_poison()
 {
@@ -108,7 +65,7 @@ void Hero::turn_left()
 	step_r = tmp_ptr;
 }
 
-//вспомогательная структура для отмотки ходов 
+// вспомогательная структура для отмотки ходов 
 struct hero_and_mark
 {
 	Hero user;
