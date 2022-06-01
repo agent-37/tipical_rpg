679158de5452ba4df9499637e1c329b7a801145d
email <maxim.dementev.04@gmail.com>
2022-06-01 20:06:59 +0300

    исправил кодировку

--- game_map.cpp
+++ game_map.cpp
@@ -1,4 +1,4 @@
-#include <iostream> 
+п»ї#include <iostream> 
 #include <algorithm> 
 #include <set> 
 #include <string> 
@@ -12,23 +12,23 @@
 #define name_file "game_map1.txt"
 using namespace std;
 
-// конструктор
+// РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
 Game_map::Game_map()
 {
-	fstream fin("input.txt", fstream::in | fstream::out | fstream::app); // позже это следует переместить в мейн
+	fstream fin("input.txt", fstream::in | fstream::out | fstream::app); // РїРѕР·Р¶Рµ СЌС‚Рѕ СЃР»РµРґСѓРµС‚ РїРµСЂРµРјРµСЃС‚РёС‚СЊ РІ РјРµР№РЅ
 	fin >> size_map;
 
-	// создание поля
+	// СЃРѕР·РґР°РЅРёРµ РїРѕР»СЏ
 	map = new int *[size_map];
 	for (int i = 0; i < size_map; i++)
 		map[i] = new int[size_map] {};
 
-	// создание второго поля
+	// СЃРѕР·РґР°РЅРёРµ РІС‚РѕСЂРѕРіРѕ РїРѕР»СЏ
 	map_2 = new int *[size_map];
 	for (int i = 0; i < size_map; i++)
 		map_2[i] = new int[size_map] {};
 
-	// чтение поля из файла
+	// С‡С‚РµРЅРёРµ РїРѕР»СЏ РёР· С„Р°Р№Р»Р°
 	for (int i = 0; i < size_map; i++)
 		for (int j = 0; j < size_map; j++)
 		{
@@ -42,7 +42,7 @@ Game_map::Game_map()
 	fin.close();
 }
 
-// деструктор
+// РґРµСЃС‚СЂСѓРєС‚РѕСЂ
 Game_map::~Game_map()
 {
 	for (int i = 0; i < size_map; i++)
@@ -62,41 +62,41 @@ Game_map::~Game_map()
 	map_2 = nullptr;
 }
 
-// изменение состояни клетки
+// РёР·РјРµРЅРµРЅРёРµ СЃРѕСЃС‚РѕСЏРЅРё РєР»РµС‚РєРё
 void Game_map::set_cell(int x, int y, int value)
 {
 	map[x][y] = value;
 }
 
-//// проверка правильности хода
+//// РїСЂРѕРІРµСЂРєР° РїСЂР°РІРёР»СЊРЅРѕСЃС‚Рё С…РѕРґР°
 //void Game_map::check_correct_move(Hero &hero)
 //{
-//	// 1 - камень (неубираемое препядствие)
-//	// 2 - дерево (убираемое препядствие)
+//	// 1 - РєР°РјРµРЅСЊ (РЅРµСѓР±РёСЂР°РµРјРѕРµ РїСЂРµРїСЏРґСЃС‚РІРёРµ)
+//	// 2 - РґРµСЂРµРІРѕ (СѓР±РёСЂР°РµРјРѕРµ РїСЂРµРїСЏРґСЃС‚РІРёРµ)
 //	if ( (map[hero.get_x()][hero.get_y()] == 1) || (map[hero.get_x()][hero.get_y()] == 2))
-//		// вернуть
+//		// РІРµСЂРЅСѓС‚СЊ
 //}
 
-// получение размера поля
+// РїРѕР»СѓС‡РµРЅРёРµ СЂР°Р·РјРµСЂР° РїРѕР»СЏ
 int Game_map::get_size_map()
 {
 	return size_map;
 }
 
-// получение содержимого клетки
+// РїРѕР»СѓС‡РµРЅРёРµ СЃРѕРґРµСЂР¶РёРјРѕРіРѕ РєР»РµС‚РєРё
 int Game_map::get_cell(int x, int y)
 {
 	return map[x][y];
 }
 
-// занулить клетку после взятия золота
+// Р·Р°РЅСѓР»РёС‚СЊ РєР»РµС‚РєСѓ РїРѕСЃР»Рµ РІР·СЏС‚РёСЏ Р·РѕР»РѕС‚Р°
 void Game_map::take_gold(Hero &hero)
 {
 	if (map[hero.get_x()][hero.get_y()] == 3)
 		map[hero.get_x()][hero.get_y()] = 0;
 }
 
-// занулить клетку после убийства monster
+// Р·Р°РЅСѓР»РёС‚СЊ РєР»РµС‚РєСѓ РїРѕСЃР»Рµ СѓР±РёР№СЃС‚РІР° monster
 void Game_map::murder_monster(Hero &hero, int num)
 {
 	if (map[hero.get_x()][hero.get_y()] == 4 || map[hero.get_x()][hero.get_y()] == 5 ||
@@ -105,7 +105,7 @@ void Game_map::murder_monster(Hero &hero, int num)
 
 }
 
-//  пометить клетку, в которой срубили дерево
+//  РїРѕРјРµС‚РёС‚СЊ РєР»РµС‚РєСѓ, РІ РєРѕС‚РѕСЂРѕР№ СЃСЂСѓР±РёР»Рё РґРµСЂРµРІРѕ
 void Game_map::cut_tree(Hero &hero)
 {
 	if (hero.step_f == &Hero::step_forward && map[hero.get_y() + 1][hero.get_x()] == 2)
@@ -118,14 +118,14 @@ void Game_map::cut_tree(Hero &hero)
 		map[hero.get_y() - 1][hero.get_x()] = 0;
 }
 
-// пометка посещённой клетки
+// РїРѕРјРµС‚РєР° РїРѕСЃРµС‰С‘РЅРЅРѕР№ РєР»РµС‚РєРё
 void Game_map::mark_visited_cell(int x, int y)
 {
 	map_2[x][y] = 1;
 }
 
-// enum препятствия, монстры, 
-// комменты
-// геттеры посещен клетки, что находится в клетке (как в draw.cpp)
-// возврат, если попал в несуществующую клетку
-// возможность изменить клетку 
+// enum РїСЂРµРїСЏС‚СЃС‚РІРёСЏ, РјРѕРЅСЃС‚СЂС‹, 
+// РєРѕРјРјРµРЅС‚С‹
+// РіРµС‚С‚РµСЂС‹ РїРѕСЃРµС‰РµРЅ РєР»РµС‚РєРё, С‡С‚Рѕ РЅР°С…РѕРґРёС‚СЃСЏ РІ РєР»РµС‚РєРµ (РєР°Рє РІ draw.cpp)
+// РІРѕР·РІСЂР°С‚, РµСЃР»Рё РїРѕРїР°Р» РІ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰СѓСЋ РєР»РµС‚РєСѓ
+// РІРѕР·РјРѕР¶РЅРѕСЃС‚СЊ РёР·РјРµРЅРёС‚СЊ РєР»РµС‚РєСѓ 
